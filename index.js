b67d8399840d917428c989a275f0f259729dd012
Ramon Mesquita <ramonmesquita113@gmail.com>
2019-10-30 21:32:01 -0300

    Classes CSS not used

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
-const regex = /\s[.]([a-zA-Z_][-]?)*/gm;
+const regexClassCSS = /\s[.]([a-zA-Z_][-]?)*/gm
+const regexClassName = /className="([\w\s-]*)"/gm
 const fs = require('fs')
-const archivePath = '/home/nomar/projects/amp_v1/components/organisms/oi-play/headerOiPlay.js'
-
+const archivePath = '/home/nomar/projects/amp_v1/components/organisms/oi-play/footerOiPlay.js'
+const classes = []
+const classNotUsed = []
 
 let m;
 const fileBuffer = fs.readFileSync(archivePath, 'utf-8')
 const str = fileBuffer.toString()
 
-while ((m = regex.exec(str)) !== null) {
-    // This is necessary to avoid infinite loops with zero-width matches
-    if (m.index === regex.lastIndex) {
-        regex.lastIndex++;
+while ((m = regexClassName.exec(str)) !== null) {
+
+
+    if (m.index === regexClassName.lastIndex) {
+        regexClassName.lastIndex++;
     }
 
-    // The result can be accessed through the `m`-variable.
-    console.log(m[0].match)
-    m.forEach((match, groupIndex) => {
-        if (match !== ' .' && match)
-            console.log(`Found match, group ${groupIndex}: ${match}`);
-    });
+    // console.log(m[1])
+
+    m[1].split(' ').map(item => {
+        if (item !== '')
+            classes.push(item)
+    })
 }
+
+let uniqueClassesUsed = [...new Set(classes)]
+uniqueClassesUsed.sort()
+console.log(uniqueClassesUsed)
+
+while ((m = regexClassCSS.exec(str)) !== null) {
+
+    if (m.index === regexClassCSS.lastIndex) {
+        regexClassCSS.lastIndex++
+    }
+
+    if (m[0] !== ' .') {
+        classCSS = m[0].substring(2, m[0].length)
+        if (!uniqueClassesUsed.includes(classCSS)) {
+            classNotUsed.push(classCSS)
+        }
+    }
+}
+
+
+let uniqueClassesNotUsed = [...new Set(classNotUsed)]
+uniqueClassesNotUsed.sort()
+// console.log(uniqueClassesNotUsed)
